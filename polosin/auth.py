5ca3f122b5f0df2cba3fb6311dab1cab4f611e3e
adjeiboyejnr <adjeiboyejnr@gmail.com>
2024-03-13 10:39:46 +0300

    modify login and signup interface and translate text to russian

--- polosin/auth.py
+++ polosin/auth.py
@@ -23,11 +23,11 @@ class Login(App):
         y = (self.winfo_screenheight() // 2) - (height // 2)
 
         self.geometry("{}x{}+{}+{}".format(width, height, x, y))
-        self.title("Login")
+        self.title(" FLOWMODEL")
         self.configure(fg_color='#232E33', pady=80, padx=0)
 
         # Login label
-        self.login = c.CTkLabel(self, text="Login", font=('Bell MT', 60))
+        self.login = c.CTkLabel(self, text="Авторизация", font=('Bell MT', 30))
         self.login.grid(row=0, pady=10)
 
         # Username
@@ -42,7 +42,7 @@ class Login(App):
         # self.username_label.grid(row=0)
 
         # Entry
-        self.username = c.CTkEntry(self.username_frame, placeholder_text='Username',text_color='#000000',
+        self.username = c.CTkEntry(self.username_frame, placeholder_text='Логин',text_color='#000000',
                                    placeholder_text_color='#000000', fg_color='#D9D9D9', border_width=0,
                                    width=200, height=40, corner_radius=30)
         self.username.grid(row=1)
@@ -54,7 +54,7 @@ class Login(App):
         self.password_frame.grid(row=2, pady=15)
 
         # Entry
-        self.password = c.CTkEntry(self.password_frame, placeholder_text='Password', show='*', text_color='#000000',
+        self.password = c.CTkEntry(self.password_frame, placeholder_text='Пароль', show='*', text_color='#000000',
                                    placeholder_text_color='#000000', fg_color='#D9D9D9', border_width=0,
                                    width=200, height=40, corner_radius=30)
         self.password.grid(row=1)
@@ -66,20 +66,20 @@ class Login(App):
 
         radio_var = tkinter.IntVar(value=0)
         # Combobox
-        self.combobox_var = c.StringVar(value="role")
-        self.combobox = c.CTkComboBox(self.frame, values=["admin", "user"], corner_radius=30,fg_color='#D9D9D9',
+        self.combobox_var = c.StringVar(value="Роль")
+        self.combobox = c.CTkComboBox(self.frame, values=["Администратор", "Исследователь"], corner_radius=30,fg_color='#D9D9D9',
                                              command=self.user_select_event, variable=self.combobox_var, width=200,
                                                   text_color='#000000',state='readonly',height=40,dropdown_font=('',20))
         self.combobox.grid(column=0, row=0, pady=10)
 
 
         # Login button
-        self.login_button = c.CTkButton(self, text='LOGIN', command=self.login_button, width=200, fg_color='#17203D',
+        self.login_button = c.CTkButton(self, text='Войти', command=self.login_button, width=200, fg_color='#17203D',
                                         corner_radius=15)
         self.login_button.grid(row=4, pady=5)
 
         # Invalid user
-        self.label = c.CTkLabel(self, text='User not found !', text_color='red')
+        self.label = c.CTkLabel(self, text='Пользователь не найден!', text_color='red')
 
         # No account
         self.no_account_frame = c.CTkFrame(self, fg_color='#232E33', width=200, height=30)
@@ -87,10 +87,10 @@ class Login(App):
         self.no_account_frame.grid(row=6)
 
         # No account label
-        self.no_account_label = c.CTkLabel(self.no_account_frame, text='Dont have an account?')
+        self.no_account_label = c.CTkLabel(self.no_account_frame, text='Нет в системе?')
         self.no_account_label.grid(column=0, row=0, padx=5)
 
-        self.signup = c.CTkLabel(self.no_account_frame, text='Sign Up', text_color='#3DB3FF', cursor='mouse')
+        self.signup = c.CTkLabel(self.no_account_frame, text='Регистрация', text_color='#3DB3FF', cursor='mouse')
         self.signup.bind("<Button-1>", self.signup_command)
         self.signup.grid(column=1, row=0)
 
@@ -118,9 +118,14 @@ class Login(App):
 
 
     def signup_command(self, event):
-        self.destroy()
-        signup = SignUp()
-        signup.mainloop()
+        try:
+            self.destroy()
+        except Exception:
+            print(Exception)
+        finally:
+            signup = SignUp()
+            signup.mainloop()
+
 
 
 class SignUp(App):
@@ -130,11 +135,11 @@ class SignUp(App):
         y = (self.winfo_screenheight() // 2) - (height // 2)
 
         self.geometry("{}x{}+{}+{}".format(width, height, x, y))
-        self.title("Login")
+        self.title("FLOWMODEL")
         self.configure(fg_color='#232E33', pady=80, padx=0)
 
         # Login label
-        self.login = c.CTkLabel(self, text="Sign up", font=('Bell MT', 60))
+        self.login = c.CTkLabel(self, text="Регистрация", font=('Bell MT', 30))
         self.login.grid(row=0, pady=10)
 
         # Username
@@ -149,7 +154,7 @@ class SignUp(App):
         # self.username_label.grid(row=0)
 
         # Entry
-        self.username = c.CTkEntry(self.username_frame, placeholder_text='Username', text_color='#000000',
+        self.username = c.CTkEntry(self.username_frame, placeholder_text='Логин', text_color='#000000',
                                    placeholder_text_color='#000000', fg_color='#D9D9D9', border_width=0,
                                    width=200, height=40, corner_radius=30)
         self.username.grid(row=1)
@@ -161,7 +166,7 @@ class SignUp(App):
         self.password_frame.grid(row=2, pady=15)
 
         # Entry
-        self.password = c.CTkEntry(self.password_frame, placeholder_text='Password', show='*', text_color='#000000',
+        self.password = c.CTkEntry(self.password_frame, placeholder_text='Пароль', show='*', text_color='#000000',
                                    placeholder_text_color='#000000', fg_color='#D9D9D9', border_width=0,
                                    width=200, height=40, corner_radius=30)
         self.password.grid(row=1)
@@ -174,14 +179,14 @@ class SignUp(App):
         radio_var = tkinter.IntVar(value=0)
 
         # combobox - User role
-        self.combobox_var = c.StringVar(value="role")
-        self.combobox = c.CTkComboBox(self.frame, values=["admin", "user"], corner_radius=30, fg_color='#D9D9D9',
+        self.combobox_var = c.StringVar(value="Роль")
+        self.combobox = c.CTkComboBox(self.frame, values=["Администратор", "Исследователь"], corner_radius=30, fg_color='#D9D9D9',
                                       command=self.user_select_event, variable=self.combobox_var, width=200,
                                       text_color='#000000', state='readonly', height=40, dropdown_font=('', 20))
         self.combobox.grid(column=0, row=0, pady=10)
 
         # Login button
-        self.login_button = c.CTkButton(self, text='SIGN UP', command=self.signup_button_click, width=200, fg_color='#17203D',
+        self.login_button = c.CTkButton(self, text='Зарегистрироваться', command=self.signup_button_click, width=200, fg_color='#17203D',
                                         corner_radius=15)
         self.login_button.grid(row=4, pady=5)
 
@@ -191,10 +196,10 @@ class SignUp(App):
         self.no_account_frame.grid(row=5)
 
         # No account label
-        self.no_account_label = c.CTkLabel(self.no_account_frame, text='Already have an account?')
+        self.no_account_label = c.CTkLabel(self.no_account_frame, text='Уже в системе?')
         self.no_account_label.grid(column=0, row=0, padx=5)
 
-        self.signup = c.CTkLabel(self.no_account_frame, text='Login', text_color='#3DB3FF', cursor='mouse')
+        self.signup = c.CTkLabel(self.no_account_frame, text='Войти', text_color='#3DB3FF', cursor='mouse')
         self.signup.bind("<Button-1>", self.login_command)
         self.signup.grid(column=1, row=0)
 
